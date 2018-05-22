e3589795e3d6099ab75e4044a7bd0805cb148a78
initxuan <907405474@qq.com>
2018-05-22 10:40:26 +0800

    实现托盘图标"左键"和"右键"功能

--- dialog.cpp
+++ dialog.cpp
@@ -19,6 +19,10 @@ Dialog::Dialog(QWidget *parent) :
     trayIcon->setToolTip(tr("单击显示系统信息\n右击显示功能菜单"));
     setBothIcons(0);
     trayIcon->setVisible(true);
+
+    setupTrayMenu();
+
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(myIconActivated(QSystemTrayIcon::ActivationReason)));
 }
 
 Dialog::~Dialog()
@@ -54,4 +58,71 @@ void Dialog::setBothIcons(int index)
     // index = 1; 有错误,显示错误图标
     (index == 0) ? setWindowIcon(iconCorrect) : setWindowIcon(iconError);
     (index == 0) ? trayIcon->setIcon(iconCorrect) : trayIcon->setIcon(iconError);
+}
+
+void Dialog::closeEvent(QCloseEvent *event)
+{
+    if(trayIcon->isVisible())
+    {
+        if(isLogin)
+            hide();
+        else
+        {
+            QMessageBox::information(this, tr("远程传输与控制系统"), tr("请登录。\n\n退出程序：请右击系统托盘图标，选择退出程序"));
+        }
+        event->ignore();
+    }
+}
+
+void Dialog::setupTrayMenu()
+{
+    // 要检索INI和设备信息后才处理
+    userMsg = new QAction(tr("当前用户："), this);
+    deviceMsg = new QAction(tr("当前设备："), this);
+    logoutAction = new QAction(tr("注销用户"), this);
+    quitAction = new QAction(tr("退出程序"), this);
+
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->addAction(userMsg);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(deviceMsg);
+    trayIconMenu->addSeparator();
+
+    trayIconMenu->addAction(logoutAction);
+    connect(logoutAction, SIGNAL(triggered()), this, SLOT(logout()));
+
+    trayIconMenu->addAction(quitAction);
+    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));     // 非常重要,必须是qApp,不能是this
+
+    trayIcon->setContextMenu(trayIconMenu);
+}
+
+void Dialog::logout()
+{
+    isLogin = false;
+    showNormal();
+}
+
+void Dialog::myIconActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch(reason){
+    case QSystemTrayIcon::Trigger:
+    case QSystemTrayIcon::DoubleClick:
+        showMessage();
+        break;
+    default:
+        break;
+    }
+}
+
+void Dialog::showMessage()
+{
+    deviceInfo.clear();
+    QString title = tr("系统状态信息");
+    deviceInfo.append(tr("1. aaa"));
+    deviceInfo.append(tr("2. bbb"));
+    int duration = 3;
+    trayIcon->showMessage(title, deviceInfo.join("\n"),     // bodyEdit->toPlainText()
+                          QSystemTrayIcon::Information,     // icon
+                          duration * 1000);
 }
--- dialog.h
+++ dialog.h
@@ -2,7 +2,16 @@
 #define DIALOG_H
 
 #include <QDialog>
+
+// 系统托盘图标相关头文件
 #include <QSystemTrayIcon>
+
+// 关闭窗口相关的头文件
+#include <QCloseEvent>
+
+// 实现功能菜单所需的头文件
+#include <QAction>
+#include <QMenu>
 
 namespace Ui {
 class Dialog;
@@ -21,15 +30,37 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    // 设置系统托盘图标和程序最小化图标
     void setBothIcons(int);
+
+    // 劫持"关闭"事件
+    void closeEvent(QCloseEvent *); // 框架默认自带函数
+
+    // 配制功能菜单,鼠标右键功能
+    void setupTrayMenu();
+
+    // 鼠标左键功能
+    void myIconActivated(QSystemTrayIcon::ActivationReason);
+
+    void logout();
+
+private:
+    void showMessage();     // 显示消息
 
 private:
     Ui::Dialog *ui;
 
-    bool isLogin;
+    bool isLogin;           // 登录状态标志
 
+    QStringList deviceInfo; // 用于显示系统消息
+
+    // 系统托盘图标
     QIcon iconCorrect, iconError;
     QSystemTrayIcon *trayIcon;
+
+    // 右击托盘显示的功能菜单
+    QAction *userMsg, *deviceMsg, *logoutAction, *quitAction;
+    QMenu *trayIconMenu;
 };
 
 #endif // DIALOG_H
